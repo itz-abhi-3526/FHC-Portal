012756dbb52ceb35e689a324c1c23326a0cdc615
Abhishek Murali <itz.abhi.3526@gmail.com>
2025-12-03 17:39:47 +0530

    Update Hero.jsx

--- FHC-PORTAL/src/components/Hero.jsx
+++ FHC-PORTAL/src/components/Hero.jsx
@@ -1,11 +1,1 @@
-import './Landing.css'
-function Header() {
-    return(
-        <div className="header">
-            <h1 className='head'>FISAT HORIZON CLUB</h1>
-            <button className='auth'>Get Started</button>
-        </div>
-    )
-}
 
-export default Header;
