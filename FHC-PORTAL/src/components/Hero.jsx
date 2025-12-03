e0ab9c9d00dddd62e759165c670a88cb17cf9be8
itz-abhi-3526 <itz.abhi.3526@gmail.com>
2025-12-03 17:38:26 +0530

    Initial commit

---
+++ FHC-PORTAL/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import './Landing.css'
+function Header() {
+    return(
+        <div className="header">
+            <h1 className='head'>FISAT HORIZON CLUB</h1>
+            <button className='auth'>Get Started</button>
+        </div>
+    )
+}
+
+export default Header;
