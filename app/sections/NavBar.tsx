6bd1a1d219a14a903e24bdb43921f5a4310e6bbb
Abhishek Murali <itz.abhi.3526@gmail.com>
2025-12-04 21:58:50 +0530

    Update NavBar.tsx

--- app/sections/NavBar.tsx
+++ app/sections/NavBar.tsx
@@ -4,75 +4,75 @@ import Container from "../components/container/Container";
 import React from "react";
 
 const NavBar = () => {
-    const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        e.preventDefault();
-        const href = e.currentTarget.href.split("#")[1];
-        window.scrollTo({
-            top: document.getElementById(href)?.offsetTop,
-            left: 0,
-            behavior: "smooth",
-        });
-    };
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const href = e.currentTarget.href.split("#")[1];
+    window.scrollTo({
+      top: document.getElementById(href)?.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
 
-    return (
-        <nav className="nowrap fixed bottom-10 left-0 right-0 z-50 my-0 mx-auto flex items-center justify-center gap-1 px-1 py-1 text-[#e4ded7] sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
-            <Container
-                width="130%"
-                height="60px"
-                color="rgba(255, 255, 255, 0.1)"
-                borderRadius={10}
-                top="0px"
-                left="0px"
-                angle={0}
-            >
-                <nav className="nowrap fixed bottom-30 left-0 right-0 z-50 my-0 mx-auto flex items-center justify-center gap-1 rounded-lg px-1 py-1 text-[#e4ded7] sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
-                    <Link
-                        href="#home"
-                        data-blobity-magnetic="false"
-                        onClick={handleScroll}
-                        aria-label="Scroll to Home Section"
-                    >
-                        <h4 className="py-2 px-2 text-[12px] sm:px-4 sm:text-[14px] md:py-1 md:px-4">
+  return (
+    <nav className="fixed bottom-10 left-0 right-0 z-50 mx-auto flex items-center justify-center text-[#e4ded7]">
+      <Container
+        width="130%"
+        height="60px"
+        color="rgba(255, 255, 255, 0.1)"
+        borderRadius={10}
+        top="0px"
+        left="0px"
+        angle={0}
+      >
+        <div className="flex items-center justify-center gap-1 px-2 py-1 rounded-lg sm:w-[383.3px] lg:w-[391.3px]">
+          <Link
+            href="#home"
+            data-blobity-magnetic="false"
+            onClick={handleScroll}
+            aria-label="Scroll to Home Section"
+          >
+            <h4 className="px-2 py-2 text-[12px] sm:px-4 sm:text-[14px] md:text-[14px]">
               HOME
-                        </h4>
-                    </Link>
+            </h4>
+          </Link>
 
-                    <Link
-                        href="#about"
-                        data-blobity-magnetic="false"
-                        onClick={handleScroll}
-                        aria-label="Scroll to About Section"
-                    >
-                        <h4 className="py-2 px-2 text-[25px] whitespace-nowrap inline-flex sm:px-4 sm:text-[14px] md:py-1 md:px-4">
-  ABOUT FHC
-</h4>
-                    </Link>
+          <Link
+            href="#about"
+            data-blobity-magnetic="false"
+            onClick={handleScroll}
+            aria-label="Scroll to About Section"
+          >
+            <h4 className="px-2 py-2 whitespace-nowrap text-[14px] sm:px-4 sm:text-[14px] md:text-[14px]">
+              ABOUT FHC
+            </h4>
+          </Link>
 
-                    <Link
-                        href="#tools"
-                        data-blobity-magnetic="false"
-                        onClick={handleScroll}
-                        aria-label="Scroll to Work Section"
-                    >
-                        <h4 className="py-2 px-2 text-[25px] whitespace-nowrap inline-flex sm:px-4 sm:text-[14px] md:py-1 md:px-4">
-  FHC PORTAL
-</h4>
-                    </Link>
+          <Link
+            href="#tools"
+            data-blobity-magnetic="false"
+            onClick={handleScroll}
+            aria-label="Scroll to Tools Section"
+          >
+            <h4 className="px-2 py-2 whitespace-nowrap text-[14px] sm:px-4 sm:text-[14px] md:text-[14px]">
+              FHC PORTAL
+            </h4>
+          </Link>
 
-                    <Link
-                        href="#contact"
-                        data-blobity-magnetic="false"
-                        onClick={handleScroll}
-                        aria-label="Scroll to Contact Section"
-                    >
-                        <h4 className="py-2 px-2 text-[12px] sm:px-4 sm:text-[14px] md:py-1 md:px-4">
+          <Link
+            href="#contact"
+            data-blobity-magnetic="false"
+            onClick={handleScroll}
+            aria-label="Scroll to Contact Section"
+          >
+            <h4 className="px-2 py-2 text-[12px] sm:px-4 sm:text-[14px] md:text-[14px]">
               CONTACT
-                        </h4>
-                    </Link>
-                </nav>
-            </Container>
-        </nav>
-    );
+            </h4>
+          </Link>
+        </div>
+      </Container>
+    </nav>
+  );
 };
 
 export default NavBar;
